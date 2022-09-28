4851073d7747719d6efaed1647c32ccd74891e1b
Rodrigo Dias <rodrigo.elfo@gmail.com>
2022-09-28 20:27:16 -0300

    fix: remove customWidgets from app-example

--- packages/app-example/src/app/styles/CustomWidgets/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as ArrayFieldTemplate } from './ArrayFieldTemplate'
-export { default as CustomCheckboxWidget } from './CustomCheckboxWidget'
-export { default as CustomCheckboxesWidget } from './CustomCheckboxesWidget'
-export { default as CustomSelectWidget } from './CustomSelectWidget'
-export { default as CustomSwitchWidget } from './CustomSwitchWidget'
-export { default as CustomTextField } from './CustomTextField'
-export { default as CustomRadioWidget } from './CustomRadioWidget'
