de8126db1b9600e5ee798f99ce2ec25dc89da6dc
Christiano Higuto <chrisshiguto@gmail.com>
2023-11-10 10:14:40 -0300

    feat: refactor container with drawer

---
+++ packages/react-material-ui/src/components/AppBar/index.ts
@@ -0,0 +1,13 @@
+import AppBarMain from './AppBarMain';
+import AppBarDrawer from './AppBarDrawer';
+import AppBarNav from './AppBarNav';
+import AppBarRoot from './AppBarRoot';
+
+const AppBar = {
+  Main: AppBarMain,
+  Drawer: AppBarDrawer,
+  Nav: AppBarNav,
+  Root: AppBarRoot,
+};
+
+export default AppBar;
