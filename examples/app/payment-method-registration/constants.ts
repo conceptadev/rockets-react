52788ebb5fc089aedd6883bffc12e6d1ef6133c2
Jefferson Silva <brnmouras@gmail.com>
2023-12-26 15:55:51 -0300

    refactor: implement masking for specific inputs

--- examples/app/payment-method-registration/constants.ts
+++ examples/app/payment-method-registration/constants.ts
@@ -1,6 +1,6 @@
 'use client';
 
-import type { RJSFSchema } from '@rjsf/utils';
+import type { RJSFSchema, UiSchema } from '@rjsf/utils';
 
 import {
   CustomTextFieldWidget,
@@ -33,13 +33,14 @@ export const schema: RJSFSchema = {
     'city',
     'state',
     'zipCode',
+    'cardNumber',
     'expirationDate',
     'cvc',
   ],
   properties: {
     fullName: { type: 'string', title: 'Full name', minLength: 3 },
     email: { type: 'string', title: 'Email', format: 'email' },
-    phone: { type: 'string', title: 'Phone', minLength: 3 },
+    phone: { type: 'string', title: 'Phone' },
     address: { type: 'string', title: 'Address', minLength: 3 },
     city: {
       title: 'City',
@@ -98,4 +99,96 @@ export const widgets = {
   TextWidget: CustomTextFieldWidget,
   EmailWidget: CustomEmailFieldWidget,
   SelectWidget: CustomSelectWidget,
+};
+
+const getMaskedPhone = (text: string) => {
+  const numericInput = text.replace(/\D/g, '');
+  const truncatedInput = numericInput.slice(0, 10);
+  const phoneNumberFormat = /^(\d{3})?(\d{3})?(\d{0,4})?$/;
+  const formattedNumber = truncatedInput.replace(
+    phoneNumberFormat,
+    (_, p1, p2, p3) => {
+      const formattedGroups = [p1 && `(${p1})`, p2 && ` ${p2}`, p3 && `-${p3}`];
+      return formattedGroups.filter(Boolean).join('');
+    },
+  );
+
+  return formattedNumber;
+};
+
+const getMaskedZipCode = (value: string) => {
+  const numericInput = value.replace(/\D/g, '');
+  const truncatedInput = numericInput.slice(0, 5);
+  const phoneNumberFormat = /^(\d{5})?$/;
+  const formattedNumber = truncatedInput.replace(phoneNumberFormat, (_, p1) => {
+    const formattedGroups = [p1 && `${p1}`];
+    return formattedGroups.filter(Boolean).join('');
+  });
+
+  return formattedNumber;
+};
+
+const getMaskedCardNumber = (value: string) => {
+  const numericInput = value.replace(/\D/g, '');
+  const truncatedInput = numericInput.slice(0, 16);
+  const phoneNumberFormat = /^(\d{4})?(\d{4})?(\d{4})?(\d{4})$/;
+  const formattedNumber = truncatedInput.replace(
+    phoneNumberFormat,
+    (_, p1, p2, p3, p4) => {
+      const formattedGroups = [
+        p1 && `${p1}`,
+        p2 && `${p2}`,
+        p3 && `${p3}`,
+        p4 && `${p4}`,
+      ];
+      return formattedGroups.filter(Boolean).join(' ');
+    },
+  );
+
+  return formattedNumber;
+};
+
+const getMaskedExpirationDate = (value: string) => {
+  const numericInput = value.replace(/\D/g, '');
+  const truncatedInput = numericInput.slice(0, 4);
+  const phoneNumberFormat = /^(\d{2})?(\d{2})?$/;
+  const formattedNumber = truncatedInput.replace(
+    phoneNumberFormat,
+    (_, p1, p2) => {
+      const formattedGroups = [p1 && `${p1}`, p2 && `${p2}`];
+      return formattedGroups.filter(Boolean).join('/');
+    },
+  );
+
+  return formattedNumber;
+};
+
+const getMaskedCVC = (value: string) => {
+  const numericInput = value.replace(/\D/g, '');
+  const truncatedInput = numericInput.slice(0, 3);
+  const phoneNumberFormat = /^(\d{3})?$/;
+  const formattedNumber = truncatedInput.replace(phoneNumberFormat, (_, p1) => {
+    const formattedGroups = [p1 && `${p1}`];
+    return formattedGroups.filter(Boolean).join('');
+  });
+
+  return formattedNumber;
+};
+
+export const uiSchema: UiSchema = {
+  phone: {
+    'ui:formatter': (value: string) => getMaskedPhone(value),
+  },
+  zipCode: {
+    'ui:formatter': (value: string) => getMaskedZipCode(value),
+  },
+  cardNumber: {
+    'ui:formatter': (value: string) => getMaskedCardNumber(value),
+  },
+  expirationDate: {
+    'ui:formatter': (value: string) => getMaskedExpirationDate(value),
+  },
+  cvc: {
+    'ui:formatter': (value: string) => getMaskedCVC(value),
+  },
 };
