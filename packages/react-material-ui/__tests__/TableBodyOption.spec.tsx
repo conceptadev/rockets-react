3c47ae28dd316060a4d34714d5316044c26c7dec
Rodrigo Dias <rodrigo.elfo@gmail.com>
2024-01-04 10:26:40 -0300

    feat: tableBodyOption tests

---
+++ packages/react-material-ui/__tests__/TableBodyOption.spec.tsx
@@ -0,0 +1,105 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { TableBodyCells } from '../src/components/Table/TableBody/TableBodyCells';
+import { TableBodyOption } from '../src/components/Table/TableBody/TableBodyOption';
+import { Table as RocketsTable } from '../src/';
+import { TableBody, TableContainer } from '@mui/material';
+import { RowProps } from '../src/components/Table/types';
+
+type GenerateRowsProps = (length: number) => RowProps[];
+
+const generateRows: GenerateRowsProps = (length: number) => {
+  const rows: RowProps[] = [];
+  for (let i = 0; i < length; i++) {
+    rows.push({ id: String(i), name: `row ${i}`, age: i });
+  }
+  return rows;
+};
+
+const customRowOptions = (row: RowProps) => [
+  { onClick: () => {}, key: row.id, text: `row ${row.id} option 0` },
+  { onClick: () => {}, key: row.id, text: `row ${row.id} option 1` },
+];
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  useSearchParams: jest.fn(),
+  usePathname: jest.fn(),
+}));
+
+describe('TableBodyCells component', () => {
+  const props = {
+    rows: generateRows(3),
+    headers: [
+      { id: 'id', label: 'ID' },
+      { id: 'name', label: 'Name' },
+      { id: 'age', label: 'Age' },
+    ],
+  };
+
+  it('should render correctly', () => {
+    const { getAllByTestId } = render(
+      <RocketsTable.Root {...props}>
+        <TableContainer>
+          <RocketsTable.Table stickyHeader variant="clean">
+            <TableBody>
+              {props.rows?.map((row) => (
+                <RocketsTable.BodyRow row={row} key={row.id}>
+                  <TableBodyOption
+                    row={row}
+                    customRowOptions={customRowOptions(row)}
+                    toggleDirection="horizontal"
+                  />
+                  <TableBodyCells row={row} />
+                </RocketsTable.BodyRow>
+              ))}
+            </TableBody>
+          </RocketsTable.Table>
+        </TableContainer>
+      </RocketsTable.Root>,
+    );
+    const menus = getAllByTestId('fade-button');
+    expect(menus).toHaveLength(3);
+  });
+
+  it('should render the TableOptions component with the correct values', async () => {
+    const { getAllByTestId, getByText, debug } = render(
+      <RocketsTable.Root {...props}>
+        <TableContainer>
+          <RocketsTable.Table stickyHeader variant="clean">
+            <TableBody>
+              {props.rows?.map((row) => (
+                <RocketsTable.BodyRow row={row} key={row.id}>
+                  <TableBodyOption
+                    row={row}
+                    customRowOptions={customRowOptions(row)}
+                    toggleDirection="vertical"
+                  />
+                  <TableBodyCells row={row} />
+                </RocketsTable.BodyRow>
+              ))}
+            </TableBody>
+          </RocketsTable.Table>
+        </TableContainer>
+      </RocketsTable.Root>,
+    );
+    const menus = getAllByTestId('fade-button');
+    expect(menus).toHaveLength(3);
+    debug();
+
+    const horizontalIcon = getAllByTestId('MoreVertIcon');
+    expect(horizontalIcon).toHaveLength(3);
+
+    fireEvent.click(menus[0]);
+
+    const item0 = getByText('row 0 option 0');
+    const item1 = getByText('row 0 option 1');
+    expect(item0).toBeInTheDocument();
+    expect(item1).toBeInTheDocument();
+  });
+});
