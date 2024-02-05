98bedf8a4b5183f009aeb023072628ba2dac0a49
Jefferson Silva <brnmouras@gmail.com>
2024-02-05 12:28:31 -0300

    refactor: add crud api

--- examples/app/layout.tsx
+++ examples/app/layout.tsx
@@ -1,16 +1,12 @@
+'use client';
+
 import type { ReactNode } from 'react';
-import type { Metadata } from 'next';
 
 import { Inter } from 'next/font/google';
 
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Examples',
-  description: 'Rockets form examples',
-};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
