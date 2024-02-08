efe8d71eaabcf22e502d77c488a6fc1e922a9a8d
Jefferson Silva <brnmouras@gmail.com>
2024-02-07 23:14:34 -0300

    refactor: implement auth forms as modules

--- examples/app/forms/login/page.tsx
+++ examples/app/forms/login/page.tsx
@@ -1,48 +1,14 @@
 'use client';
 
-import type { IChangeEvent } from '@rjsf/core';
+import dynamic from 'next/dynamic';
 
-import { useState } from 'react';
-import { Container, Card } from '@mui/material';
-import { SimpleForm } from '@concepta/react-material-ui';
-import useDataProvider, { useQuery } from '@concepta/react-data-provider';
-
-import { type FormData, schema } from './constants';
-
-const uri = '/api/login';
+const AuthModule = dynamic(
+  () => import('@concepta/react-material-ui/dist/modules/auth'),
+  { ssr: false },
+);
 
 const Login = () => {
-  const [formData, setFormData] = useState<FormData>({
-    email: '',
-    password: '',
-  });
-
-  const { post } = useDataProvider();
-
-  const { execute: login } = useQuery((body) => post({ uri, body }), false);
-
-  const handleSubmit = async (values: IChangeEvent<FormData>) => {
-    setFormData(values.formData as FormData);
-
-    await login(values.formData);
-  };
-
-  return (
-    <Container maxWidth="xs" sx={{ textAlign: 'center', padding: '48px 0' }}>
-      <h1>Login</h1>
-      <Card sx={{ marginTop: '48px', padding: '24px' }}>
-        <SimpleForm
-          form={schema}
-          initialData={formData}
-          onSubmit={handleSubmit}
-          onError={(err) =>
-            // eslint-disable-next-line no-console
-            console.log('errors', err)
-          }
-        />
-      </Card>
-    </Container>
-  );
+  return <AuthModule route="signIn" moduleProps={{ logoSrc: '/next.svg' }} />;
 };
 
 export default Login;
