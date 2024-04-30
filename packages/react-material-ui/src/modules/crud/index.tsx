a0f7f8da925eafbf4b07a1fb50b746d3a60bb7ae
Jefferson Silva <brnmouras@gmail.com>
2024-04-30 12:41:05 -0300

    feat: add handler to action buttons click

--- packages/react-material-ui/src/modules/crud/index.tsx
+++ packages/react-material-ui/src/modules/crud/index.tsx
@@ -72,6 +72,35 @@ const CrudModule = (props: ModuleProps) => {
     },
   });
 
+  const changeCurrentFormData = (
+    direction: 'previous' | 'next',
+    data: SelectedRow,
+  ) => {
+    const { data: tableData } = useTableReturn;
+    const currentItemIndex = tableData.findIndex(
+      (item: SelectedRow) => item.id === data.id,
+    );
+
+    let newItemIndex = 0;
+
+    if (
+      (direction === 'previous' && currentItemIndex === 0) ||
+      (direction === 'next' && currentItemIndex === tableData.length - 1)
+    ) {
+      return;
+    }
+
+    if (direction === 'previous') {
+      newItemIndex = currentItemIndex - 1;
+    }
+
+    if (direction === 'next') {
+      newItemIndex = currentItemIndex + 1;
+    }
+
+    setSelectedRow(tableData[newItemIndex] as Record<string, unknown>);
+  };
+
   const FormComponent = useMemo(() => {
     switch (props.formContainerVariation) {
       case 'drawer':
@@ -166,6 +195,8 @@ const CrudModule = (props: ModuleProps) => {
               setSelectedRow(null);
               setDrawerViewMode(null);
             }}
+            onPrevious={(data) => changeCurrentFormData('previous', data)}
+            onNext={(data) => changeCurrentFormData('next', data)}
             {...enhancedFormProps}
           >
             {enhancedFormProps.children}
