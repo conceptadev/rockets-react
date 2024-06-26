00e763f12d3e5da1746a9223d04fc1ff811a1056
Jefferson Silva <brnmouras@gmail.com>
2024-06-26 17:23:04 -0300

    feat: extend resource adding methods

--- packages/react-material-ui/src/utils/i18n/index.ts
+++ packages/react-material-ui/src/utils/i18n/index.ts
@@ -1,7 +1,12 @@
 import type { i18n as I18n } from 'i18next';
 
 import i18n from 'i18next';
-import { initReactI18next, useTranslation, setI18n } from 'react-i18next';
+import {
+  initReactI18next,
+  useTranslation,
+  setI18n,
+  getI18n,
+} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import locales from './locales';
@@ -21,6 +26,18 @@ i18n
     },
   });
 
-export type { I18n };
+const addResource = getI18n().addResource;
 
-export { i18n, useTranslation, setI18n };
+const addResources = getI18n().addResources;
+
+const addResourceBundle = getI18n().addResourceBundle;
+
+export {
+  type I18n,
+  i18n,
+  useTranslation,
+  addResource,
+  addResources,
+  addResourceBundle,
+  setI18n,
+};
