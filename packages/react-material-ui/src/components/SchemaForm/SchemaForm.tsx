5ec2fd38fcd1af66f7e102e49935e981cd989437
Rodrigo Dias <rodrigo.elfo@gmail.com>
2024-07-24 08:32:49 -0300

    feat: schemaform - tsdocs - info from readme

--- packages/react-material-ui/src/components/SchemaForm/SchemaForm.tsx
+++ packages/react-material-ui/src/components/SchemaForm/SchemaForm.tsx
@@ -20,38 +20,102 @@ import { ObjectFieldTemplate } from '../../styles/CustomTemplates';
  * SchemaForm component props.
  */
 export type SchemaFormProps = Omit<FormProps, 'schema' | 'validator'> & {
-  /** JSON schema for the form */
+  /** Object that defines the structure of the form, containing information about field names, properties and formatting. Comprehensive guide available at the [schema docs](https://json-schema.org/learn/getting-started-step-by-step). */
   schema: RJSFSchema;
-  /** Validator function for the form */
+  /** Validation helper for the form structure. _@rjsf/validator-ajvx_, provided by [ajv](https://github.com/ajv-validator/ajv), is generally used for this purpose. It is implemented on the form via the [HTML5 Validation](https://rjsf-team.github.io/react-jsonschema-form/docs/usage/validation#html5-validation) */
   validator?: FormProps['validator'];
-  /** Advanced properties for custom configurations */
+  /** Additional schema for fields that are out of the default _string_, _number_, _integer_, _object_, _array_, _boolean_ and _null_ set for the form schema. The field set for advanced properties contain the same types as the default one, but add other fields such as _email_, _password_, _select_, _radio_, _checkbox_, _checkboxes_ and _switch_. */
   advancedProperties?: Record<string, AdvancedProperty>;
-  /** Title for the submit button */
+  /** Title for the form submit button */
   buttonTitle?: string;
-  /** Custom button component */
+  /** Custom component for the form submit button */
   buttonComponent?: ReactNode;
-  /** Custom title component */
+  /** The title of the form, usually displayed on top of the fields */
   title?: ReactNode;
-  /** Mapper function for advanced properties */
+  /** Custom mapper for the advanced properties. The default mapper for the form component is described below this table */
   advancedPropertiesMapper?: AdvancedPropertiesMapper;
 };
 
 /**
- * The Form component is a wrapper around the React JSON Schema Form library,
- * customized with additional features such as advanced properties mapping,
- * custom UI schema generation, and form data merging.
+ * Advanced Properties Mapper default structure
+ * ```json
+ * {
+ *   "string": "string",
+ *   "email": "string",
+ *   "password": "string",
+ *   "array": "array",
+ *   "select": "string",
+ *   "radio": "string",
+ *   "checkbox": "boolean",
+ *   "checkboxes": "array",
+ *   "switch": "boolean"
+ * }
+ * ```
  *
+ * The rest of the SchemaForm props extend from [react-jsonschema-form](https://rjsf-team.github.io/react-jsonschema-form/docs/).
+ */
+
+/**
+ * The SchemaForm component allows forms to be created based
+ * on schemas passed via props. Internally, it implements the
+ * [react-jsonschema-form](https://rjsf-team.github.io/react-jsonschema-form/docs/) pattern.
+ *
+ * @see {@link [react-jsonschema-form documentation](https://rjsf-team.github.io/react-jsonschema-form/docs/)}
  * @param props - SchemaForm component props
+ *
+ * The following example describes the full composition that mounts the SchemaForm component:
  *
  * @example
  * ```tsx
- * <Form
- *   schema={mySchema}
- *   formData={myFormData}
- *   advancedProperties={myAdvancedProperties}
+ * import SchemaForm from '@concepta/material-ui';
+ * import { Button } from '@mui/material';
+ * import validator from '@rjsf/validator-ajv6';
+ *
+ * <SchemaForm.Form
+ *   schema={{
+ *     title: 'Register',
+ *     description: 'A schema form example.',
+ *     type: 'object',
+ *     required: ['firstName', 'lastName', 'email', 'password'],
+ *     properties: {
+ *       firstName: {
+ *         type: 'string',
+ *         title: 'First name',
+ *       },
+ *       lastName: {
+ *         type: 'string',
+ *         title: 'Last name',
+ *       },
+ *       email: {
+ *         type: 'string',
+ *         title: 'Email',
+ *       },
+ *       password: {
+ *         type: 'string',
+ *         title: 'Password',
+ *         minLength: 3,
+ *       },
+ *     },
+ *   }}
+ *   uiSchema={{
+ *     email: {
+ *       'ui:widget': 'email',
+ *     },
+ *     password: {
+ *       'ui:widget': 'password',
+ *     },
+ *   }}
+ *   validator={validator}
  * >
- *   <Button type="submit">Submit</Button>
- * </Form>
+ *   <Button
+ *     type="submit"
+ *     variant="contained"
+ *     disabled={isLoadingCreation || isLoadingEdit}
+ *     sx={{ flex: 1, mr: 2 }}
+ *   >
+ *     Submit
+ *   </Button>
+ * </SchemaForm.Form>
  * ```
  */
 const Form = ({
