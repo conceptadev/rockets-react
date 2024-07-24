da5951cf639faa4903f704da379c9e7b3d82d4c0
Rodrigo Dias <rodrigo.elfo@gmail.com>
2024-07-24 07:40:47 -0300

    feat: storybook - Text - add mui info

--- packages/react-material-ui/src/components/Text/Text.tsx
+++ packages/react-material-ui/src/components/Text/Text.tsx
@@ -3,24 +3,26 @@ import Typography, { TypographyProps } from '@mui/material/Typography';
 
 /**
  * The Text component is a wrapper around the Material-UI Typography component
- * with a default fontWeight of 300.
+ * with a default fontWeight of 300. It's props extend from [Material UI's Typography](https://mui.com/material-ui/api/Typography/#props)
+ * component props, so every prop is interchangeable between those two.
  *
+ * @see {@link [MUI Typography Component](https://mui.com/material-ui/react-typography/)}
  * @see [Storybook - Text](https://storybook.rockets.tools/?path=/docs/text)
  *
  * @example
  * ```tsx
- * <Text variant="h6" fontWeight="400">
+ * <Text variant="h6" fontWeight="400" fontSize='18px'>
  *   Sample Text
  * </Text>
  * ```
  *
- * @param props - Typography component props
+ * @param typographyProps - MUI {@link [TypographyProps](https://mui.com/material-ui/api/Typography/#props)}
  */
-const Text = (props: TypographyProps) => {
-  const { children, fontWeight = '300' } = props;
+const Text = (typographyProps: TypographyProps) => {
+  const { children, fontWeight = '300' } = typographyProps;
 
   return (
-    <Typography fontWeight={fontWeight} {...props}>
+    <Typography fontWeight={fontWeight} {...typographyProps}>
       {children}
     </Typography>
   );
